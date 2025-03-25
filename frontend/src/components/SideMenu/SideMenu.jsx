214464133627d05e0455e20e8d5c3b88aabc1461
MyNameIsJedd-305 <michelgarcia950528@icloud.com>
2025-03-25 11:30:14 -0400

    Setting Up Community Smiles Customization

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -4,11 +4,12 @@ import './SideMenu.css';
 const SideMenu = () => {
     return (
         <div className="side-menu">
+            <img className="main-image" src='/main_logo.png'/>
             <ul>
                 <li><Link className="menu-item" to="/">Dashboard</Link></li>
                 <li><Link className="menu-item" to="/patients">Patients</Link></li>
-                <li><Link className="menu-item" to="/appointments">Appointments</Link></li>
-                <li><Link className="menu-item" to="/settings">Settings</Link></li>
+                {/* <li><Link className="menu-item" to="/appointments">Appointments</Link></li>
+                <li><Link className="menu-item" to="/settings">Settings</Link></li> */}
             </ul>
         </div>
     );
